ae7724e43cfe2eb21f6461430f51da2062db29e7
sahil-binekar <sahil.binekar@cryptextechnologies.com>
2022-03-29 11:04:48 +0530

    Added Sales count to to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_123801) do
+ActiveRecord::Schema.define(version: 2022_03_29_052931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_123801) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "sales_count", default: 0, null: false
   end
 
 end
